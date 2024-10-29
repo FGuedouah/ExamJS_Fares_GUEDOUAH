98398bac7a127dccbe8db50bd3ba5d025fe3afe5
FGuedouah <fguedouah.sio@gmail.com>
2024-10-29 22:34:16 +0100

    panier et quantité qui marchent

--- script.js
+++ script.js
@@ -24,27 +24,28 @@ function afficherProduits(data) {
     produits.appendChild(divProduitCard);
 
   });
-  let quantity = 1;
+  var quantityObj = {
+    quantity : 1  
+  }
   const btnsAjouterPanier = document.querySelectorAll(".ajouterPanier");
   btnsAjouterPanier.forEach((btn) => {
     btn.addEventListener("click", function () {
       let index = this.getAttribute("data-index");
-      // si le produit est dans le tableau ajouter 1 a quantity
-      // sinon push le produit dans le tableau
-      console.log("data index", data[index])
-      console.log(panierProduit)
-      panierProduit.forEach(produit => {
-          console.log("LAAAAAAA ",produit)
-      });
       
-      
-      if(panierProduit.find(produit => produit === data[index])){
-          console.log("y a deja")
-      }else{
+           // Vérifier si le produit est déjà dans le panier
+           let produitDansPanier = panierProduit.find(
+            produit => produit[0].nom_produit === data[index].nom_produit
+          );
 
-      }
-      panierProduit.push([data[index], quantity]);
-      
+          if (produitDansPanier) {
+            // Si le produit existe déjà, augmenter la quantité
+            produitDansPanier[1].quantity += 1;
+            console.log("Produit déjà dans le panier, quantité augmentée :", produitDansPanier[1].quantity);
+          } else {
+            // Sinon, ajouter le produit avec une quantité de 1
+            panierProduit.push([data[index], { quantity: 1 }]);
+            console.log("Produit ajouté au panier :", data[index].nom_produit);
+          }  
       
       console.log(panierProduit)
       localStorage.setItem("panierProduits", JSON.stringify(panierProduit));
