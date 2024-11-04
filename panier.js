76fa38994368772c08f7b04f9989f806639193c3
FGuedouah <fguedouah.sio@gmail.com>
2024-11-05 00:10:22 +0100

    formulaire de commande

--- panier.js
+++ panier.js
@@ -1,21 +1,21 @@
-let panierProduit = JSON.parse(localStorage.getItem("panierProduits")); // recuepere les elements du panier
+let panierProduit = JSON.parse(localStorage.getItem("panierProduits")) || []; // Récupère les éléments du panier ou un tableau vide // recuepere les elements du panier
 let divPanier = document.getElementById("panier");
 let prixTotalPanier = 0; // Prix total du panier
 
 function afficherPanier() {
-    divPanier.innerHTML = ""; // Réinitialise l'affichage du panier
-    prixTotalPanier = 0; // Réinitialise le total
-    
-    // Si le panier n'est pas vide
-    if (panierProduit.length > 0) {
-      panierProduit.forEach((produit, index) => {
-        let prixProduit = parseFloat(produit[0].prix.replace("€", "")); // Supprime le symbole et convertit en nombre
-        let prixProduitTotal = prixProduit * produit[1].quantity; // Prix total pour la quantité de ce produit
-  
-        prixTotalPanier += prixProduitTotal; // Ajouter le prix total du produit au prix total du panier
-        
-        // Affiche chaque produit
-        divPanier.innerHTML += `
+  divPanier.innerHTML = ""; // Réinitialise l'affichage du panier
+  prixTotalPanier = 0; // Réinitialise le total
+
+  // Si le panier n'est pas vide
+  if (panierProduit.length > 0 && panierProduit) {
+    panierProduit.forEach((produit, index) => {
+      let prixProduit = parseFloat(produit[0].prix.replace("€", "")); // Supprime le symbole et convertit en nombre
+      let prixProduitTotal = prixProduit * produit[1].quantity; // Prix total pour la quantité de ce produit
+
+      prixTotalPanier += prixProduitTotal; // Ajouter le prix total du produit au prix total du panier
+
+      // Affiche chaque produit
+      divPanier.innerHTML += `
         <div class="card mb-3">
           <div class="row g-0">
             <div class="col md-4 sm-3">
@@ -38,27 +38,26 @@ function afficherPanier() {
           </div>
         </div>
         `;
-      });
-  divPanier.innerHTML += `
+    });
+    divPanier.innerHTML += `
     <div class="row">
         <h2 class="col-8">Total</h2>
-        <h4 class="col-4">${prixTotalPanier} €</h4>
+        <h4 class="col-4">${prixTotalPanier.toFixed(2)} €</h4>
     </div>
     `;
-}
-   else {
-    console.log("le panier est vide dzada");
-  divPanier.innerHTML += `
+  } else {
+    let divForm = document.getElementById('form-commande')
+    divForm.innerHTML = "";
+    divPanier.innerHTML += `
       <div class="text-center">
           <h2>Panier vide</h2>
       </div>
-    `
+    `;
   }
 
-   // Ajoute les événements aux boutons après chaque rendu
-   gererQuantite();
+  // Ajoute les événements aux boutons après chaque rendu
+  gererQuantite();
 }
-
 
 // Fonction pour ajouter les événements aux boutons + et -
 function gererQuantite() {
@@ -96,3 +95,21 @@ function gererQuantite() {
 afficherPanier();
 
 
+function validateContact(e){
+    const nom = document.getElementById("nom").value;
+    const prenom = document.getElementById("prenom").value;
+    const email = document.getElementById("email").value;
+    const telephone = document.getElementById("telephone").value;
+    const adresse = document.getElementById("adresse").value;
+    const utilisationDonnees = document.getElementById("checkboxDonnee").checked;
+    const caseRGPD = document.getElementById("checkboxRGPD").checked
+
+  if (!nom || !prenom || !email || !telephone || !adresse || !utilisationDonnees|| !caseRGPD) {
+    alert("Remplissez tous les champs !!!");
+    event.preventDefault();
+  } else {
+    panierProduit.length = 0;
+    localStorage.setItem("panierProduits", JSON.stringify(panierProduit));
+    alert("Votre commande a bien été validé");
+  }
+}
